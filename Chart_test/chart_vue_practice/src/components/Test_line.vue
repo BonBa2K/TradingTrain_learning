7cf4fd60fcf97b3f236028173dbfab0913d7b5c1
unknown <z890657@gmail.com>
2023-04-23 16:08:47 +0800

    script set up applied

---
+++ Chart_test/chart_vue_practice/src/components/Test_line.vue
@@ -0,0 +1,34 @@
+<template>
+    <highcharts class="chart" :options="chartOptions"></highcharts>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            chartOptions: {
+                chart: {
+                    type: 'line'
+                },
+                title: {
+                    text: 'Fruit Consumption'
+                },
+                xAxis: {
+                    categories: ['Apples', 'Bananas', 'Oranges']
+                },
+                yAxis: {
+                    title: {
+                        text: 'Fruit eaten'
+                    }
+                },
+                series: [{
+                    name: 'Jane',
+                    data: [1, 0, 4]
+                }, {
+                    name: 'John',
+                    data: [5, 7, 3]
+                }]
+            },
+        }
+    }
+}
+</script>
